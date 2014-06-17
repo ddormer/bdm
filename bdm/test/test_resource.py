d57b29094291800d76c9dc3f40cf3a9902055d79
jayess <jayess@jayess.za.net>
2014-06-17 20:50:40 +0200

    Add test for serverStats.

--- bdm/test/test_resource.py
+++ bdm/test/test_resource.py
@@ -10,3 +10,39 @@ class TestDonationAPI(TestCase):
     """
     Tests for L{bdm.resource.DonationAPI}.
     """
+    def setUp(self):
+        self.store = Store()
+        self.api = DonationAPI(self.store, 'nothing')
+
+
+    def test_serverStats(self):
+        def _cb(result):
+            self.assertEqual([{
+                'server_name': 'Test Server',
+                'map': 'testmap',
+                'player_count': 8,
+                'max_players': 16
+            }], result)
+
+        servers = [
+            ['1.1.1.1', 27015]
+        ]
+
+        return self.api.serverStats(servers, querier=MockServerQuerier).addCallback(_cb)
+
+
+
+class MockServerQuerier(object):
+    def __init__(self, address, timeout=5.0):
+        self.host = address[0]
+        self.port = address[1]
+        self.timeout = timeout
+
+
+    def get_info(self):
+        return {
+            'server_name': 'Test Server',
+            'map': 'testmap',
+            'player_count': 8,
+            'max_players': 16
+        }
